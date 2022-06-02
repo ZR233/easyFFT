ee332ef5a418eef8fe1d19d8899057c8738f306f
周睿 <zhourui818@126.com>
2022-06-02 14:27:45 +0800

    清理代码

--- src/Exception.hpp
+++ src/Exception.hpp
@@ -3,19 +3,24 @@
 #define Exception_HPP
 
 
-#include <stdexcept>
+#include <exception>
 #include "include/easyFFT.h"
 #include <string>
 
-class Exception: public std::runtime_error{
-
-
+class Exception: public std::exception{
 public:
     Exception(const char * msg, FFT_ERROR_CODE error_code):
-        std::runtime_error(msg),
-        error_code(error_code){
+        std::exception(),
+        error_code(error_code),
+        msg(msg){
     }
     const FFT_ERROR_CODE error_code;
+    const char * what() const noexcept override{
+        return msg.data();
+    }
+
+private:
+    std::string msg;
 };
 
 #endif
